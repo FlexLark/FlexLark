8b86eb1d45e444b44c9471697f1a09a5b4886fe6
Eeymoo <eeymoo@xmail.fun>
2024-07-26 00:46:28 +0800

    feat: 添加了元数据存储类和接口以支持歌曲、播放列表、专辑和歌手信息的获取、设置和删除操作

--- src/db/MetadataStore.ts
+++ src/db/MetadataStore.ts
@@ -1,4 +1,5 @@
 import BaseStore from "./BaseStore";
+import { SongMetadata, PlaylistMetadata, AlbumMetadata, SingerMetadata } from "../types/Metadata";
 
 export default class MetadataStore extends BaseStore {
   protected static instance: MetadataStore | null;
@@ -16,6 +17,9 @@ export default class MetadataStore extends BaseStore {
     CREATE UNIQUE INDEX IF NOT EXISTS playlist_metadata (
 
     );
+    CREATE UNIQUE INDEX IF NOT EXISTS catgory_metadata (
+
+    );
     `;
   }
   protected getDbPath(): string {
@@ -29,4 +33,33 @@ export default class MetadataStore extends BaseStore {
     MetadataStore.instance = new MetadataStore();
     return MetadataStore.instance;
   }
+
+  
+  public async getSongMetadata(): Promise<SongMetadata> { }
+  public async getSongMetadataById(id: string): Promise<SongMetadata> { }
+  public async setSongMetadataById(id: string, metadata: SongMetadata): Promise<void> { }
+  public async deleteSongMetadataByIds(id: string): Promise<void> { }
+  public async getSongMetadataBySinger(id: string): Promise<SongMetadata[]> { }
+  public async getSongMetadataByAlbum(id: string): Promise<SongMetadata[]> { }
+  public async getSongMetadataByPlaylist(id: string): Promise<SongMetadata[]> { }
+  public async getSongMetadataByCategory(id: string): Promise<SongMetadata[]> { }
+
+  public async getPlaylistMetadata(id: string): Promise<PlaylistMetadata> { }
+  public async setPlaylistMetadata(id: string, metadata: PlaylistMetadata): Promise<void> { }
+  public async deletePlaylistMetadataByIds(id: string): Promise<void> { }
+
+  public async getAlbumMetadata(id: string): Promise<AlbumMetadata> { }
+  public async setAlbumMetadata(id: string, metadata: AlbumMetadata): Promise<void> { }
+  public async deleteAlbumMetadataByIds(id: string): Promise<void> { }
+  public async getAlbumMetadataBySinger(id: string): Promise<AlbumMetadata[]> { }
+
+
+  public async getSingerMetadata(id: string): Promise<SingerMetadata> { }
+  public async setSingerMetadata(id: string, metadata: SingerMetadata): Promise<void> { }
+  public async deleteSingerMetadataByIds(id: string): Promise<void> { }
+
+  public async getCategoryMetadata(id: string): Promise<CategoryMetadata> { }
+  public async setCategoryMetadata(id: string, metadata: CategoryMetadata): Promise<void> { }
+  public async deleteCategoryMetadataByIds(id: string): Promise<void> { }
 }
+
