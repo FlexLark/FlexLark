9f7fa8499aa8041fed2c7359106d1a4375377797
Eeymoo <eeymoo@xmail.fun>
2024-07-27 00:24:06 +0800

    feat: 修改默认语言设置为默认语言

--- i18n.js
+++ i18n.js
@@ -2,26 +2,28 @@
 import i18n from 'i18next';  
 import { initReactI18next } from 'react-i18next';  
 import en from './locales/en';  
-import zh from './locales/zh-cn';  
-  
+import zhCN from './locales/zh-cn';  
+
+let language = navigator.language || navigator.browserLanguage || navigator.userLanguage;
+console.log(language);
 const resources = {  
   en: {  
     translation: en,  
   },  
-  zh: {  
-    translation: zh,  
+  'zh-CN': {  
+    translation: zhCN,  
   },  
-};  
+};
   
 i18n  
   .use(initReactI18next)  
-  .init({  
+  .init({
     resources,  
-    lng: 'zh', // 默认语言  
-    fallbackLng: 'zh', // 备用语言  
+    lng: language, // 默认语言  
+    fallbackLng: 'en', // 备用语言  
     interpolation: {  
       escapeValue: false, // React 默认会转义，这里设置为 false  
-    },  
-  });  
+    },
+  });
   
 export default i18n;
