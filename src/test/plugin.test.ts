bc29d76fef8e5c51a6ecb4f77f740b5724ede120
Eeymoo <eeymoo@xmail.fun>
2024-07-30 04:00:58 +0800

    feat(core): 更新插件管理器以支持ICoreContext接口

--- src/test/plugin.test.ts
+++ src/test/plugin.test.ts
@@ -2,18 +2,19 @@ import { describe, expect, test, vi } from "vitest";
 import { PluginManager } from "../core/manager/PluginManager";
 import { IPlugin } from "../core/interface/IPlugin";
 import Core from "../core";
+import { ICoreContext } from "../core/interface/ICoreContext";
 
 describe("PluginManager", () => {
   test("register", async () => {
     const core = new Core();
-    const pluginManager = new PluginManager(console, core);
+    const pluginManager = new PluginManager(console, core as unknown as ICoreContext);
     const plugins: IPlugin = {
       id: "test",
       name: "test",
       author: "test",
       description: "test",
       version: "test",
-      install: vi.fn((core: Core): Core => {
+      install: vi.fn((core: ICoreContext): ICoreContext => {
         // You can add custom behavior here if needed
         return core;
       })
@@ -26,16 +27,24 @@ describe("PluginManager", () => {
     expect(pluginManager.plugins[0]).toBe(plugins);
     expect(plugins.install).toHaveBeenCalled();
   });
+
+  test("register error", async () => {
+    const core = new Core();
+    const pluginManager = new PluginManager(console, core as unknown as ICoreContext);
+    expect(() => pluginManager.register({} as IPlugin)).toThrowError();
+    expect(pluginManager.pluginMap.size).toBe(0);
+  });
+
   test("destroy", async () => {
     const core = new Core();
-    const pluginManager = new PluginManager(console, core);
+    const pluginManager = new PluginManager(console, core as unknown as ICoreContext);
     const plugins: IPlugin = {
       id: "test",
       name: "test",
       author: "test",
       description: "test",
       version: "test",
-      install: vi.fn((core: Core): Core => {
+      install: vi.fn((core: ICoreContext): ICoreContext => {
 
         return core;
       })
@@ -48,16 +57,36 @@ describe("PluginManager", () => {
     expect(pluginManager.plugins.length).toBe(0);
   })
 
+  test("destroy error", async () => {
+    const core = new Core();
+    const pluginManager = new PluginManager(console, core as unknown as ICoreContext);
+    const plugins: IPlugin = {
+      id: "",
+      name: "",
+      author: "",
+      description: "",
+      version: "",
+      install: function (core: ICoreContext): ICoreContext {
+        return core;
+      },
+      destroy: function (core: ICoreContext) {
+        throw new Error();
+      }
+    }
+    expect(() => pluginManager.destroy()).toThrowError();
+    expect(pluginManager.pluginMap.size).toBe(0);
+  })
+
   test("update", async () => {
     const core = new Core();
-    const pluginManager = new PluginManager(console, core);
+    const pluginManager = new PluginManager(console, core as unknown as ICoreContext);
     const plugins: IPlugin = {
       id: "test",
       name: "test",
       author: "test",
       description: "test",
       version: "test",
-      install: vi.fn((core: Core): Core => {
+      install: vi.fn((core: ICoreContext): ICoreContext => {
         // You can add custom behavior here if needed
         return core;
       })
@@ -68,5 +97,12 @@ describe("PluginManager", () => {
 
     expect(pluginManager.pluginMap.get(plugins.name)).toBe(plugins);
     expect(pluginManager.pluginMap.size).toBe(1);
+  })
+
+  test("update error", async () => {
+    const core = new Core();
+    const pluginManager = new PluginManager(console, core as unknown as ICoreContext);
+    expect(() => pluginManager.update()).toThrowError();
+    expect(pluginManager.pluginMap.size).toBe(0);
   })
 });
